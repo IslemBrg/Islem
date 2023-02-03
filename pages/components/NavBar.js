85c8f434a2b8fcd14316d40e02ac90610a9bcfe3
IslemBrg <islem.bargaoui@istic.ucar.tn>
2023-02-03 14:14:20 +0100

    responsive navbar

--- pages/components/NavBar.js
+++ pages/components/NavBar.js
@@ -32,9 +32,7 @@ export default function NavBar() {
           >
       <div className="flex flex-col items-center w-16 h-full overflow-hidden text-gray-700 bg-gray-100 rounded">
 		<Link onClick={()=> {setSideBar(false)}} className="flex items-center justify-center mt-3" href="/">
-			<svg className="w-8 h-8 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-				<path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z" />
-			</svg>
+    <Image src="/Images/dedsec logo.png" width={30} height={100} className="h-6 mr-3 sm:h-9" alt="Flowbite Logo"/>
 		</Link>
 		<div className="flex flex-col items-center mt-3 border-t border-gray-300">
 			<Link onClick={()=> {setSideBar(false)}} className="flex items-center justify-center w-12 h-12 mt-2 rounded hover:bg-gray-300" href="/">
