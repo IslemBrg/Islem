c6ade2408e99a4f76e656d82facc5ae9e98e693b
IslemBrg <islem.bargaoui@istic.ucar.tn>
2023-02-03 14:11:06 +0100

    responsive navbar

--- pages/components/NavBar.js
+++ pages/components/NavBar.js
@@ -1,44 +1,122 @@
 import React from 'react'
 import Image from 'next/image'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Link from 'next/link';
 import { AnimatePresence, motion } from 'framer-motion';
 
 export default function NavBar() {
   const [contacts, setcontacts] = useState(false);
+  const [isMobile, setisMobile] = useState(false);
+  const [SideBar, setSideBar] = useState(false)
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 760) {
+        setisMobile(true);
+      }
+    };
+    handleResize();
+  }, []);
   return (
     <>
-    <nav className="border-radius: 0.25rem bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-    <div className="container flex flex-wrap items-center justify-between mx-auto">
-        <a href="/" className="flex items-center">
-            <Image src="/Images/dedsec logo.png" width={60} height={100} className="h-6 mr-3 sm:h-9" alt="Flowbite Logo"/>
-          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Retr0 Development</span>
-      </a>
-      <div className="flex md:order-2">
-          <Link href="signup"><button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Try sign up</button></Link>
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+    {isMobile ? (
+    <div style={{display:"flex"}}>
+      {SideBar ? (<div style={{display:"flex"}}>
+        <AnimatePresence>
+          <motion.div
+           initial={{ x: -100 }}
+           animate={{ x: 0 }}
+           exit={{ x: -100 }}
+           transition={{ duration: 0.5 }}
+          >
+      <div className="flex flex-col items-center w-16 h-full overflow-hidden text-gray-700 bg-gray-100 rounded">
+		<Link onClick={()=> {setSideBar(false)}} className="flex items-center justify-center mt-3" href="/">
+			<svg className="w-8 h-8 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+				<path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z" />
+			</svg>
+		</Link>
+		<div className="flex flex-col items-center mt-3 border-t border-gray-300">
+			<Link onClick={()=> {setSideBar(false)}} className="flex items-center justify-center w-12 h-12 mt-2 rounded hover:bg-gray-300" href="/">
+				<svg className="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+				 	<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+				</svg>
+			</Link>
+			<Link onClick={()=> {setSideBar(false)}} className="flex items-center justify-center w-12 h-12 mt-2 rounded" href="About">
+				<svg className="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+					<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+				</svg>
+			</Link>
+			<a onClick={() => {setcontacts(!contacts)}} className="flex items-center justify-center w-12 h-12 mt-2 rounded" href="#">
+      <svg aria-hidden="true" focusable="false" class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="1em" height="1em"><path d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z"/></svg>
+			</a>
+			
+		</div>
+		<Link onClick={() => {setSideBar(!SideBar)}} className="flex items-center justify-center w-16 h-16 mt-auto bg-gray-200 hover:bg-gray-300" href="signup">
+			<svg className="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+				<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+			</svg>
+		</Link>
+	</div>
+  </motion.div></AnimatePresence>
+  <AnimatePresence>
+          <motion.div
+           initial={{ x: -100 }}
+           animate={{ x: 0 }}
+           exit={{ x: -100 }}
+           transition={{ duration: 0.5 }}
+          >
+  <button onClick={()=> {setSideBar(!SideBar)}} className="text-black bg-white  font-medium rounded-lg text-sm px-2 py-2.5 mr-2 dark:hover:bg-blue-700 " type="button">
+  <svg width="24" height="24"  fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect x="2" y="11" width="20" height="2" fill="#333"/>
+    <rect x="2" y="6" width="20" height="2" fill="#333"/>
+    <rect x="2" y="16" width="20" height="2" fill="#333"/>
+  </svg>
+</button></motion.div></AnimatePresence>
+</div>):(
+  <div>
+        <button onClick={()=> {setSideBar(!SideBar)}} className="text-black bg-white  font-medium rounded-lg text-sm px-2 py-2.5 mr-2 dark:hover:bg-blue-700 " type="button">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="2" y="11" width="20" height="2" fill="#333"/>
+            <rect x="2" y="6" width="20" height="2" fill="#333"/>
+            <rect x="2" y="16" width="20" height="2" fill="#333"/>
+          </svg>
+        </button>
+      </div>)}
     </div>
-    <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-         <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <Link href="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" >Home</Link>
-         </li>
-         <li>
-             <Link href="About" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</Link>
-         </li>
-         <li>
-             <a href="#" onClick={() => {setcontacts(!contacts)}} className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
-         </li>
-         </ul>
-        </div>
-    </div>
-    </nav>
+    ):(
+      <nav className="border-radius: 0.25rem bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+      <div className="container flex flex-wrap items-center justify-between mx-auto">
+          <a href="/" className="flex items-center">
+              <Image src="/Images/dedsec logo.png" width={60} height={100} className="h-6 mr-3 sm:h-9" alt="Flowbite Logo"/>
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Retr0 Development</span>
+        </a>
+        <div className="flex md:order-2">
+            <Link href="signup"><button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Try sign up</button></Link>
+      </div>
+      <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+           <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <Link href="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" >Home</Link>
+           </li>
+           <li>
+               <Link href="About" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</Link>
+           </li>
+           <li>
+               <a href="#" onClick={() => {setcontacts(!contacts)}} className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+           </li>
+           </ul>
+          </div>
+      </div>
+      </nav>
+    )}
     <div>
     <AnimatePresence>
     {contacts && (
        <motion.div
-        initial={{ y: -130 }}
+        initial={{ y: -1000 }}
         animate={{ y: 0 }}
-        exit={{ y: -130 }}
+        exit={{ y: -1000 }}
         transition={{ duration: 0.5 }}
      >
         <div style={{ borderRadius:"50px", background: 'lightblue', padding: '20px' ,position:'absolute',top:70,width:"100%",zIndex:3}}>
@@ -75,11 +153,21 @@ export default function NavBar() {
               </div>
             </button></Link>
           </div>
-          
-          <div style={{marginTop:5,display:"flex",alignItems:"center",justifyContent: 'center'}}>
+          {isMobile ?(
+            <div style={{marginTop:5,alignItems:"center",justifyContent: 'center', textAlign:"center"}}>
+          <Link href="tel:+21622901462"><div style={{display:"flex"}}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="w-5 h-5"><path fill="currentColor" d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"/></svg>&nbsp;22 901 462 </div></Link>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <Link href="mailto:islem.bargaoui.si@gmail.com"><div style={{display:"flex"}}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512" className="w-5 h-5"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"/></svg>&nbsp;islem.bargaoui.si@gmail.com </div></Link>
+          <br/>
+          <button onClick={()=> {setcontacts(!contacts)}} style={{fontSize:20}} className="text-black bg-blue-100  font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:hover:bg-blue-700 " type="button">
+            ⇑
+          </button>
+        </div>
+          ):(
+            <div style={{marginTop:5,display:"flex",alignItems:"center",justifyContent: 'center'}}>
             <Link href="tel:+21622901462"><div style={{display:"flex"}}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="w-5 h-5"><path fill="currentColor" d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"/></svg>&nbsp;22 901 462 </div></Link>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <Link href="mailto:islem.bargaoui.si@gmail.com"><div style={{display:"flex"}}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512" className="w-5 h-5"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"/></svg>&nbsp;islem.bargaoui.si@gmail.com </div></Link>
           </div>
+        )}
         </div>
         </motion.div>
       )}
