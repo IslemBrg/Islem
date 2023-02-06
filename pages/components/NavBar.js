5cc0771b98b6621881d579bfb0b1761e71433bcb
Islem Bargaoui <93430376+IslemBrg@users.noreply.github.com>
2023-02-06 16:47:18 +0100

    removed (under construction) signup

--- pages/components/NavBar.js
+++ pages/components/NavBar.js
@@ -51,7 +51,7 @@ export default function NavBar() {
 			</a>
 			
 		</div>
-		<Link onClick={() => {setSideBar(!SideBar)}} className="flex items-center justify-center w-16 h-16 mt-auto bg-gray-200 hover:bg-gray-300" href="signup">
+		<Link onClick={() => {setSideBar(!SideBar)}} className="flex items-center justify-center w-16 h-16 mt-auto bg-gray-200 hover:bg-gray-300" href="#">
 			<svg className="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 				<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
 			</svg>
@@ -91,7 +91,7 @@ export default function NavBar() {
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Retr0 Development</span>
         </a>
         <div className="flex md:order-2">
-            <Link href="signup"><button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Try sign up</button></Link>
+            <Link href="#"><button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Try sign up</button></Link>
       </div>
       <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
            <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
